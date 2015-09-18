d40c965e8788d73c066fa11b2f63990f0624c9f5
Guillaume Petitpierre <guillaume.petitpierre@he-arc.ch>
2015-09-18 15:16:54 +0200

    Added Java

--- Java/Example1/src/example1/Example1.java
+++ Java/Example1/src/example1/Example1.java
@@ -16,7 +16,7 @@ public class Example1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello, world.");
+        System.out.println("Hello, world...");
     }
-    
+
 }
